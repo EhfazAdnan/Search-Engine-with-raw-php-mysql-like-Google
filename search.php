d62458bdf99cf10bbb29d874179126a018d08197
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2019-11-06 03:01:07 +0600

    Complete search page header with search type tabs style

--- search.php
+++ search.php
@@ -4,6 +4,12 @@
     }else{
         exit("You must enter a search term");
     }
+
+    if(isset($_GET["type"])){
+       $type = $_GET["type"];
+    }else{
+       $type = "sites";
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -43,13 +49,13 @@
         <div class="tabsContainer">
            <ul class="tabList">
 
-              <li>
+              <li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
                 <a href='<?php echo "search.php?term=$term&type=sites";?>'>
                   Sites 
                 </a>
               </li>
 
-              <li>
+              <li class="<?php echo $type == 'images' ? 'active' : '' ?>">
                 <a href='<?php echo "search.php?term=$term&type=images";?>'>
                   Images
                 </a>
