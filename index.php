e88f1270d8c4e1d2605ba42f32b8cd0c8ab14ce5
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2019-11-05 22:44:27 +0600

    Sample logo and a search form

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Doodle</title>
+</head>
+<body>
+
+    <div class="logoContainer">
+       <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+    </div>
+
+    <div class="searchContainer">
+
+       <form action="search.php" nethod="GET">
+           <input type="text" class="searchBox" name="term" placeholder="Search here..">
+           <input type="submit" class="searchButton" value="Search">
+       </form>
+       
+    </div>
+</body>
+</html> 
