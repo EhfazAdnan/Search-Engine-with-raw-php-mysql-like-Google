2a7fc98ce7026228d2720548b43ff0af5a28d102
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2019-11-06 18:54:25 +0600

    Get links from other websites

--- classes/DomDocumentParser.php
+++ classes/DomDocumentParser.php
@@ -1,8 +1,25 @@
 <?php
 
 class DomDocumentParser {
+
+    private $doc;
+
     public function __construct($url) {
-        echo "URL: $url";
+        
+        $options = array(
+            'http'=>array('method'=>"GET", 'header'=>"User-Agent: doodleBot/0.1\n")
+        );
+
+        $context = stream_context_create($options);
+
+        $this->doc = new DomDocument();
+
+        @$this->doc->loadHTML(file_get_contents($url, false, $context));
+
+    }
+
+    public function getlinks() {
+        return $this->doc->getElementsByTagName("a");
     }
 }
 
--- crawl.php
+++ crawl.php
@@ -3,9 +3,16 @@ include("classes/DomDocumentParser.php");
 
 function followLinks($url){
     $parser = new DomDocumentParser($url);
+
+    $linkList = $parser->getLinks();
+
+    foreach($linkList as $link) {
+        $href = $link->getAttribute("href");
+        echo $href . "<br>";
+    }
 }
 
-$startUrl = "http://www.reecekenney.com";
+$startUrl = "http://www.bbc.com";
 followLinks($startUrl);
 
 ?>
